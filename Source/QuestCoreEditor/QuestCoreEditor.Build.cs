aeef64eefb9dd4bd2bdb40508baa6452801b8a19
Alijimpa <alibadpa007@gmail.com>
2026-07-12 16:39:47 +0330

    Create Plugin with Editor Module

---
+++ Source/QuestCoreEditor/QuestCoreEditor.Build.cs
@@ -0,0 +1,28 @@
+using UnrealBuildTool;
+
+public class QuestCoreEditor : ModuleRules
+{
+	public QuestCoreEditor(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicIncludePaths.Add(ModuleDirectory);
+		PrivateIncludePaths.Add(ModuleDirectory);
+
+		PublicDependencyModuleNames.AddRange(new string[]
+		{
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"QuestCoreRuntime"
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[]
+		{
+			"UnrealEd",
+			"AssetTools",
+			"Slate",
+			"SlateCore"
+		});
+	}
+}
