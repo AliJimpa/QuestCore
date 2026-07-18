ab3aaf1754b46e6e4ef973d1cbc40577b7dc0b2a
Alijimpa <alibadpa007@gmail.com>
2026-07-18 20:18:48 +0330

    Remove Sharid Style from Module

--- Source/QuestCoreEditor/Module/QuestCoreEditorModule.h
+++ Source/QuestCoreEditor/Module/QuestCoreEditorModule.h
@@ -2,16 +2,11 @@
 
 #include "CoreMinimal.h"
 #include "Modules/ModuleManager.h"
-#include "AssetTypeCategories.h"
 
-class FSlateStyleSet;
 
 class FQuestCoreEditorModule : public IModuleInterface
 {
 public:
 	virtual void StartupModule() override;
 	virtual void ShutdownModule() override;
-
-private:
-	TSharedPtr<FSlateStyleSet> QuestCoreStyle;
 };
